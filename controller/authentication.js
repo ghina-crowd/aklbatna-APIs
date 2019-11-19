83cd672258f020c8004d79ea520c67d22b69d563
umar9122 <muhammed@crowd-ae.com>
2019-11-19 16:55:47 +0400

    coboney api 19 november 2019

--- controller/authentication.js
+++ controller/authentication.js
@@ -645,6 +645,7 @@ router.post('/update_reset_pass', function(req,res){
 
                 authenticationService.update_pass(password.otp,password.password).then(user => {
 
+
                     resolve(user);
 
                     if(user == null){
