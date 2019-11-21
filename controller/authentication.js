780e41a131d2049d8627ab0668e2e04eaa565d5c
umar9122 <muhammed@crowd-ae.com>
2019-11-21 10:29:20 +0400

    coboney api 20 november 2019

--- controller/authentication.js
+++ controller/authentication.js
@@ -9,7 +9,6 @@ var authenticationValidator=require('../validator/authentication.js');
 const multer = require('multer');
 var config = require('../constant/config.js');
 var bcrypt = require('bcryptjs');
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './uploads')
@@ -576,19 +575,19 @@ router.put('/resend_code', function (req, res) {
                         html: '<p>Your OTP here ' + user.otp + '</p>'// plain text body
                     };
 
-                            transporter.sendMail(mailOptions, function (err, info) {
-                                if(err)
-                                    console.log(err)
-                                else
-                                    console.log(info);
-                            });
+                    transporter.sendMail(mailOptions, function (err, info) {
+                        if (err)
+                            console.log(err)
+                        else
+                            console.log(info);
+                    });
 
-                            res.json({
-                                status: statics.STATUS_SUCCESS,
-                                code: codes.SUCCESS,
-                                message: trans_message.RESEND,
-                                data: user,
-                            });
+                    res.json({
+                        status: statics.STATUS_SUCCESS,
+                        code: codes.SUCCESS,
+                        message: trans_message.RESEND,
+                        data: user,
+                    });
 
                 }, error => {
                     reject(error);
@@ -602,164 +601,167 @@ router.put('/resend_code', function (req, res) {
             reject(error);
         });
 
-
     }
 
+});
+
 //reset password
-router.post('/sent_otp_by_email', function (req, res) {
+    router.post('/sent_otp_by_email', function (req, res) {
 
-    var trans_message = language(req.headers.language);
+        var trans_message = language(req.headers.language);
 
-    var errors = validationResult(req);
-    if (errors.array().length == 0) {
-        var creqentials = req.body;
+        var errors = validationResult(req);
+        if (errors.array().length == 0) {
+            var creqentials = req.body;
 
-        if (creqentials.email == '') {
+            if (creqentials.email == '') {
+                res.json({
+                    status: statics.STATUS_FAILURE,
+                    code: codes.FAILURE,
+                    message: trans_message.EMPTY_FIELD_EMAIL,
+                    data: null
+                });
+            } else {
+
+                return new Promise(function (resolve, reject) {
+                    authenticationService.check_email(creqentials.email).then(user => {
+                        resolve(user);
+                        if (!user) {
+
+                            res.json({
+                                status: statics.STATUS_FAILURE,
+                                code: codes.INVALID_DATA,
+                                message: trans_message.INVALID_EMAIL,
+                                data: user
+                            });
+
+                        } else {
+
+                            authenticationService.update_otp(user.email).then(user => {
+                                resolve(user);
+                                var transporter = nodemailer.createTransport({
+                                    service: 'gmail',
+                                    auth: {
+                                        user: 'muhammad.umer9122@gmail.com',
+                                        pass: 'Addidas9122334455?'
+                                    }
+                                });
+
+                                const mailOptions = {
+                                    from: 'muhammad.umer9122@gmail.com', // sender address
+                                    to: user.email, // list of receivers
+                                    subject: 'Subject of your email', // Subject line
+                                    html: '<p>Your code for reset password ' + user.otp + '</p>'// plain text body
+                                };
+
+                                transporter.sendMail(mailOptions, function (err, info) {
+                                    if (err)
+                                        console.log(err)
+                                    else
+                                        console.log(info);
+                                });
+
+                                res.json({
+                                    status: statics.STATUS_SUCCESS,
+                                    code: codes.SUCCESS,
+                                    message: trans_message.DATA_FOR_RESET,
+                                    data: user,
+                                });
+
+                            }, error => {
+                                reject(error);
+                            });
+
+
+                        }
+
+
+                    }, error => {
+                        reject(error);
+                    });
+                });
+
+            }
+        } else {
             res.json({
                 status: statics.STATUS_FAILURE,
-                code: codes.FAILURE,
-                message: trans_message.EMPTY_FIELD_EMAIL,
-                data: null
+                code: codes.INVALID_DATA,
+                message: trans_message.INVALID_DATA,
+                data: errors.array()
             });
-        } else {
 
-            return new Promise(function (resolve, reject) {
-                authenticationService.check_email(creqentials.email).then(user => {
-                    resolve(user);
-                    if (!user) {
+        }
+    });
 
-                        res.json({
-                            status: statics.STATUS_FAILURE,
-                            code: codes.INVALID_DATA,
-                            message: trans_message.INVALID_EMAIL,
-                            data: user
-                        });
+// update reset password
+    router.post('/reset_password', function (req, res) {
+        var trans_message = language(req.headers.language);
 
-                    } else {
+        var errors = validationResult(req);
+        if (errors.array().length == 0) {
+            var data = req.body;
 
-                        authenticationService.update_otp(user.email).then(user => {
-                            resolve(user);
-                            var transporter = nodemailer.createTransport({
-                                service: 'gmail',
-                                auth: {
-                                    user: 'muhammad.umer9122@gmail.com',
-                                    pass: 'Addidas9122334455?'
-                                }
+            if (data.password == '') {
+                res.json({
+                    status: statics.STATUS_FAILURE,
+                    code: codes.FAILURE,
+                    message: trans_message.EMPTY_FIELD_PASS,
+                    data: null
+                });
+            } else if (data.otp == '') {
+                res.json({
+                    status: statics.STATUS_FAILURE,
+                    code: codes.FAILURE,
+                    message: trans_message.EMPTY_FIELD_OTP,
+                    data: null
+                });
+            } else {
+
+                return new Promise(function (resolve, reject) {
+
+                    authenticationService.update_pass(data.otp, data.password).then(user => {
+
+                        resolve(user);
+
+                        if (user == null) {
+                            res.json({
+                                status: statics.STATUS_FAILURE,
+                                code: codes.FAILURE,
+                                message: trans_message.INVALID_OTP,
+                                data: user,
                             });
-
-                            const mailOptions = {
-                                from: 'muhammad.umer9122@gmail.com', // sender address
-                                to: user.email, // list of receivers
-                                subject: 'Subject of your email', // Subject line
-                                html: '<p>Your code for reset password ' + user.otp + '</p>'// plain text body
-                            };
-
-                            transporter.sendMail(mailOptions, function (err, info) {
-                                if(err)
-                                    console.log(err)
-                                else
-                                    console.log(info);
-                            });
-
+                        } else {
                             res.json({
                                 status: statics.STATUS_SUCCESS,
                                 code: codes.SUCCESS,
-                                message: trans_message.DATA_FOR_RESET,
+                                message: trans_message.CHANGE_PASS,
                                 data: user,
                             });
-
-                            }, error => {
-                            reject(error);
-                        });
+                        }
 
 
-                    }
-
+                    }, error => {
+                        reject(error);
+                    });
 
                 }, error => {
                     reject(error);
                 });
-            });
 
-        }
-    } else {
-        res.json({
-            status: statics.STATUS_FAILURE,
-            code: codes.INVALID_DATA,
-            message: trans_message.INVALID_DATA,
-            data: errors.array()
-        });
 
-    }
-});
-
-// update reset password
-router.post('/reset_password', function (req, res) {
-    var trans_message = language(req.headers.language);
-
-    var errors = validationResult(req);
-    if (errors.array().length == 0) {
-        var data = req.body;
-
-        if (data.password == '') {
-            res.json({
-                status: statics.STATUS_FAILURE,
-                code: codes.FAILURE,
-                message: trans_message.EMPTY_FIELD_PASS,
-                data: null
-            });
-        } else if (data.otp == '') {
-            res.json({
-                status: statics.STATUS_FAILURE,
-                code: codes.FAILURE,
-                message: trans_message.EMPTY_FIELD_OTP,
-                data: null
-            });
+            }
         } else {
-
-            return new Promise(function (resolve, reject) {
-
-                authenticationService.update_pass(data.otp, data.password).then(user => {
-
-                    resolve(user);
-
-                    if (user == null) {
-                        res.json({
-                            status: statics.STATUS_FAILURE,
-                            code: codes.FAILURE,
-                            message: trans_message.INVALID_OTP,
-                            data: user,
-                        });
-                    } else {
-                        res.json({
-                            status: statics.STATUS_SUCCESS,
-                            code: codes.SUCCESS,
-                            message: trans_message.CHANGE_PASS,
-                            data: user,
-                        });
-                    }
-
-
-                }, error => {
-                    reject(error);
-                });
-
-            }, error => {
-                reject(error);
+            res.json({
+                status: statics.STATUS_FAILURE,
+                code: codes.INVALID_DATA,
+                message: trans_message.INVALID_DATA,
+                data: errors.array()
             });
 
-
         }
-    } else {
-        res.json({
-            status: statics.STATUS_FAILURE,
-            code: codes.INVALID_DATA,
-            message: trans_message.INVALID_DATA,
-            data: errors.array()
-        });
+    });
 
-    }
-});
+
 
 
 module.exports = router;
